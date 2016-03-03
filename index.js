64fedb347b1905d23f0ccd5911f4a9c19025dcb7
Kostya Golikov <johnysilver7@gmail.com>
2016-03-03 04:22:22 +0300

    First version with bars

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('app')
 const BrowserWindow = require('browser-window')
 
+require('electron-reload')(__dirname)
+
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
-    width: 800, height: 600
+    width: 1024, height: 768
   })
 
   mainWindow.loadURL('file://' + __dirname + '/index.html')
