7b68ea58cfa548b05663fb9ee349951ce7fd739c
Kostya Golikov <johnysilver7@gmail.com>
2016-03-10 22:56:15 +0300

    Several styling improvements like text color and margins, isolating drawing in a function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('electron-reload')(__dirname)
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
-    width: 1024, height: 768
+    width: 1024, height: 512
   })
 
   mainWindow.loadURL('file://' + __dirname + '/index.html')
